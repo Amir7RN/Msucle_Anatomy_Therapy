ff49c3290adf9ca2dbe4b1690e7609396c112925
Amirreza Naseri <133896290+Amir7RN@users.noreply.github.com>
2026-05-09 13:12:34 -0400

    Move landing gate out of App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { LandingPage } from './components/landing/LandingPage'
+import { useAtlasStore } from './store/atlasStore'
 import './index.css'
+
+function Root() {
+  const params = new URLSearchParams(window.location.search)
+  const showAtlas = params.has('atlas')
+  const appUrl = `${import.meta.env.BASE_URL}?atlas=1`
+  const diagnosticUrl = `${import.meta.env.BASE_URL}?atlas=1&diagnostic=1`
+
+  if (!showAtlas) {
+    return <LandingPage atlasUrl={appUrl} diagnosticUrl={diagnosticUrl} />
+  }
+
+  return <AtlasEntry diagnosticRequested={params.has('diagnostic')} />
+}
+
+function AtlasEntry({ diagnosticRequested }: { diagnosticRequested: boolean }) {
+  useEffect(() => {
+    if (!diagnosticRequested) return
+
+    const state = useAtlasStore.getState()
+    if (!state.diagnosticMode) state.toggleDiagnosticMode()
+    state.setTriageOpen(true)
+  }, [diagnosticRequested])
+
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
